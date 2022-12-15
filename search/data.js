3e76a10b75c88ba5fdd7c6e132f084d30d24b18d
maku77 <maku77@users.noreply.github.com>
2022-12-15 17:12:35 +0000

    deploy: a4d7b7931e6c04a8fd93b129f3f2ac513ebf5671

--- search/data.js
+++ search/data.js
@@ -1,5 +1,17 @@
 var data = [
 {
+url: "/p/d8s6fpz/",
+title: "Deno プログラミングをはじめる",
+date: "2022-12-12T00:00:00Z",
+body: "Deno プログラミングをはじめる Deno とは Deno（発音: ディーノ）は、Node.js の作者であるライアン・ダール (Ryan Dahl) 氏が作成した JavaScript/TypeScript の実行環境です。 Deno という名前は、Node を逆にしたネーミングになっています。 Node.js での反省を活かした改善が行われており、今後は Node.js ではなく Deno で作成されるアプリケーションが増えていくと思われます。 Deno には次のような特徴があります。 シンプルな 1 つの実行ファイル（deno コマンド）で提供 ネイティブに TypeScript 実行をサポート（tsc コマンドのインストールが不要） 各種開発ツールを deno コマンドに標準搭載 deno fmt \u0026hellip; フォーマッター deno lint \u0026hellip; Lint ツール deno test \u0026hellip; テストランナー deno bundle \u0026hellip; バンドラー（1 つの JS や実行ファイルにまとめる） package.json で依存モジュールを管理する必要がない Promise ベースの API 実装 Rust 実装による高速動作（当初は Golang 実装だった） npm モジュールのサポート（2022 年に追加） 公式のサーバレス環境 Deno Deploy が提供されている Node.js の時代は、TypeScript 環境の準備や ESLint、Prettier の設定など、本質的ではないことに時間を取られがちでしたが、Deno であれば最初から全部入りなので、時間を有効活用できて幸せな人生を送ることができます。 Golang や Rust の開発体験のよさに、TypeScript/JavaScript が追いついた感じですね。 Deno のインストール Deno（deno コマンド）のインストールは、下記ページの説明に従えば簡単に終わります。 Installation | Manual | Deno 例えば、macOS/Linux や Window 環境では次のようにコマンド実行すれば、数秒でインストールは完了します。 deno はホームディレクトリ以下 (~/.deno/bin) にインストールされるので、管理者権限で実行する必要はありません（バイナリが 1 つ配置されるだけです）。 macOS/Linux の場合 $ curl -fsSL https://deno.land/x/install/install.sh | sh Windows の場合 (PowerShell) $ irm https://deno.land/install.ps1 | iex deno コマンドが実行できるようになっていれば OK です。 $ deno --version deno 1.28.3 (release, x86_64-pc-windows-msvc) v8 10.9.194.5 typescript 4.8.3 一度 deno コマンドをインストールしてしまえば、その後のバージョンアップは deno コマンド自身のサブコマンドで行えます。 deno のバージョンを更新する場合 $ deno upgrade # 最新バージョンへ更新 $ deno upgrade --version 1.28.3 # バージョンを指定（バージョンダウンも簡単） V8 エンジンと TypeScript 環境も同時に更新されるので、deno のバージョンだけ気にしていれば済みます。 とっても楽です。 Deno で Hello World hello.ts console.log(\u0026#34;Hello World\u0026#34;); Deno の Hello World プログラムです。 TypeScript コードで書かれていますが、JavaScript へトランスパイルする必要はなく、deno run コマンドで直接実行できます。 $ deno run hello.ts Hello World ここでは、ローカルの .ts ファイルを実行しましたが、次のように Web 上の .ts ファイルを実行することもできます。 $ deno run https://deno.land/std@0.168.0/examples/welcome.ts Welcome to Deno! これで、Deno プログラミングの準備が整いました！ ٩(๑❛ᴗ❛๑)۶ わーぃ"
+},
+{
+url: "/p/ujqinda/",
+title: "プログラミング",
+date: "2022-12-12T00:00:00Z",
+body: "プログラミング"
+},
+{
 url: "/p/io4gs6h/",
 title: "Certbot で Let's Encrypt の SSL 証明書を取得する（Web サーバーの HTTPS 対応）",
 date: "2022-12-03T00:00:00Z",
@@ -16,12 +28,6 @@ url: "/p/5s5jvfz/",
 title: "GraphQL スキーマ仕様: スキーマ定義の基本",
 date: "2022-09-14T00:00:00Z",
 body: "GraphQL スキーマ仕様: スキーマ定義の基本 GraphQL スキーマとは GraphQL スキーマは、GraphQL API サーバーがどんなクエリを受け付けてくれるかを示すもので、GraphQL Schema Language という独自言語 (DSL) で記述します。 スキーマは型情報だけを定義したものであり、サーバーの実装ロジックは考慮しません（各型のデータを返すための具体的な実装は「リゾルバー」と呼ばれており、こちらはプログラミング言語に依存した実装を行います）。 GraphQL スキーマは、クライアントのニーズを把握して、クライアントファーストで考えるのがポイントです。 サーバーのバックエンドで使用する技術（RDB など）に影響されたスキーマ定義をしてはいけません。 ☝️ .graphql vs .graphqls GraphQL スキーマドキュメントを独立したファイルとして作成する場合は、ファイルの拡張子を .graphqls あるいは .graphql とするのが一般的です。 .graphqls の方は「GraphQL のスキーマ」を表し、.graphql の方は「GraphQL のクエリドキュメント」を表すので明確に分けるべきだという主張もあります。 迷ったら、スキーマファイルには .graphqls を使っておけば間違いないと思います。 スキーマ定義のベストプラクティスに関しては、こちら にまとめています。 オブジェクト型 スキーマの中で基本的な要素となるのはオブジェクト型で、type キーワードを使って次のように定義します。 typeオブジェクト型の名前{フィールド名:型フィールド名:型フィールド名:型}これは、GraphQL クエリによって取得可能なデータの構造を表しています。 例えば、下記は書籍情報を表すオブジェクト型の例です。 typeBook{id:ID!year:Inttitle:String!authors:[String!]tags:[String!]!}Book オブジェクトが持つフィールドは、フィールド名: 型 という形式で定義します。 型には、スカラー型、列挙型、リスト、オブジェクト型などを指定できます。 オブジェクト型のフィールドとして、別のオブジェクト型を指定することができますが、末端のオブジェクト型はスカラー型のフィールドのみで構成されている必要があります。 すべての型は デフォルトで Nullable であり、Non-null なフィールドであることを示すためには、型名の末尾に ! を付ける必要があります。 上記の例の場合、各フィールドの定義は次のような意味を持ちます。 id: ID! id フィールドは ID 型（組み込みのスカラー型のひとつ）であり、null になることはない。 year: Int year フィールドは整数型であり、null になる可能性がある。 title: String! title フィールドは文字列型であり、null になることはない。 authors: [String!] authors フィールドは文字列を要素に持つリストであり、各要素が null になることはない。ただし、authors フィールド自体は null になる可能性がある。 tags: [String!]! tags フィールドは文字列を要素に持つリストであり、各要素が null になることはない。tags フィールド自体も null になることはない（少なくとも空リストが返される）。 クエリ要求のエントリーポイント（Query 型） Query 型のフィールドを定義すると、GraphQL クエリのエントリポイント（GraphQL クライアントがトップレベルで指定するフィールド）として指定できるようになります。 例えば、次の Query 型は、すべての書籍を取得するためのフィールド (books) と、指定した ID の書籍を取得するためのフィールド (book) を定義しています。 スキーマ定義の例 typeQuery{books:[Book!]!book(id:ID!):Book} books フィールドの型は [Book!]! なので、Book オブジェクトのリストが返ってきます。 book フィールドの型は Book なので、単一の Book オブジェクト、あるいは null が返ってきます。 このようなスキーマを実装する GraphQL サーバーは、クライアントからの次のようなクエリを処理できることを意味します。 クエリの例 queryQueryBooks{books{idtitle}book(id:\u0026#34;a1b2c3d\u0026#34;){idtitle}} ひとつの query 内に複数のフィールド (books と book）を指定していることに注目してください。 このように、GraphQL では異なる情報を一度のクエリでまとめて取得できるようになっています。 変更要求のエントリーポイント（Mutation 型） GraphQL におけるデータの変更要求は、ミューテーション処理 (mutation) を送信することで行います。 クライアントが要求可能なミューテーション処理の一覧は、Mutation 型のフィールドとして定義します。 次のスキーマでは、Mutation 型のフィールドとして createBook と deleteBook を定義しています。 スキーマ定義の例 typeMutation{createBook(title:String!):BookdeleteBook(id:String!):Boolean!} GraphQL クライアントは、次のように mutation { ... } 要求のトップレベル・フィールドとして、createBook 操作や deleteBook 操作を指定できます。 ミューテーション要求の例 mutationCreateBook{createBook(title:\u0026#34;My book\u0026#34;){idtitle}} query 操作と同様に、各種 mutation 操作の戻り値として任意のオブジェクト型を返せるので、上記のようにデータの作成要求と同時に、生成されたデータの情報を取得することができます。 参考: GraphQL スキーマ仕様: mutation を定義する（データ更新 API）"
-},
-{
-url: "/p/ujqinda/",
-title: "プログラミング",
-date: "2022-08-10T00:00:00Z",
-body: "プログラミング"
 },
 {
 url: "/p/esbs9o5/",
@@ -1588,6 +1594,12 @@ url: "/p/napwb4e/",
 title: ".proto の文法: サービス型 (service)",
 date: "2022-12-13T00:00:00Z",
 body: ".proto の文法: サービス型 (service) Protoco Buffers の サービス型 は、クライアントとサーバー間の通信方法 (RPC メソッド群) を定義するための型で、.proto ファイルの中で service キーワードを使って定義します。 次の例では、Echo メソッドを持つ EchoService サービス型を定義しています。 // Echo メソッドを持つ EchoService の定義 service EchoService { rpc Echo (EchoRequest) returns (EchoResponse);}// Echo に送るリクエストメッセージの定義 message EchoRequest { string message = 1; optional string payload = 2;}// Echo が返すレスポンスメッセージの定義 message EchoResponse { string message = 1;}RPC の通信プロトコルは、独自で実装してしまうことも可能ですが、多くの場合は Google が作った gRPC というプロトコルを使います。 というより、gRPC による通信を実現するために、Protocol Buffers (protobuf) によるシリアライズを使用することになるというケースが多いと思います。 protoc コマンド本体には、gRPC 用のスタブコードを生成する機能は入っていないので、protoc のプラグイン（protoc-gen-go-grpc など）を入れてコード生成することになります。 参考: Go 言語で gRPC 通信してみる（Echo サーバー＆クライアント）"
+},
+{
+url: "/p/dp4r2wz/",
+title: "Deno プログラミング関連メモ",
+date: "2022-12-12T00:00:00Z",
+body: "Deno プログラミング関連メモ"
 },
 {
 url: "/p/c9sar9p/",
